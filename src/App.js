2a7c045839e67a194b7931dde1876603db0558e1
yeojin15 <k.yeojin15@gmail.com>
2022-11-26 22:25:10 +0900

    feat: 전체 구현 완료

--- src/App.js
+++ src/App.js
@@ -1,38 +1,75 @@
 import { useState } from 'react';
 import './App.css';
+import Box from './component/Box';
 
-//! 왠만한거는 state 쓸거고, 잠깐 저장해놔야 하는 값만 변수 쓸거다~
+// 1. 박스 2개 (타이틀, 사진, 결과)
+// 2. 가위바위보 버튼
+// 3. 버튼 클릭하면 클릭한 값이 박스에 보임
+// 4. 컴퓨터는 랜덤하게 선택
+// 5. 3번과 4번의 결과를 가지고 승패 보여줌!
+// 6. 승패 결과에 따라 테두리 색상 변경
+
+const choice = {
+  rock: {
+    name : 'Rock',
+    img: 'https://yt3.ggpht.com/ytc/AMLnZu9BSithrPLlzAZWRPRHCtEYQfVX0M0aQ7QAU9AS=s900-c-k-c0x00ffffff-no-rj'
+  },
+  scissors: {
+    name: 'Scissors',
+    img: 'https://blog.kakaocdn.net/dn/HfURw/btqXKvOTNWK/gWTwPXEg9QzSV0ilOuwuak/img.png'
+  },
+  paper: {
+    name: 'Paper',
+    img: 'https://blog.kakaocdn.net/dn/bmjB2s/btqXHhp6kpG/TH14W4U612SxKo9uuR2sB0/img.png'
+  }
+}
 
 function App() {
-  let counter = 0;
+  const [userSelect, setUserSelect] = useState(null)  //! null은 초기값 
+  const [pcSelect, setPcSelect] = useState(null)
+  const [result, setResult] = useState("")
 
-  const [counter2, setCounter2] = useState(0); // ! 매개변수는 초기값
+  const play = (userChoice) => {
+    // user 선택
+    setUserSelect(choice[userChoice])
 
-  const increase = ()=>{
-    counter += 1 // * 카운터는 왜 계속 1이냐면 리렌더링 하느라 0부터 시작해서 1 더한 그것만 나와버림~!
+    // pc 랜덤선택
+    let pcChoice = randomChoice();
+    setPcSelect(pcChoice)
 
-    setCounter2(counter2+1) // ! 비동기적으로 작동한대... 그래서 콘솔찍으면 바로 전 값이 나와버린다.
-    // * 변수는 바꾸면 바로 적용되지만, 함수가 끝날 때까지 기다렸다가 실행을 하게 되므로 비동기적 작동이라서 값이 바로 전 값이 출력된다.
-
-    console.log('counter',counter) // 1
-    console.log('counter2 state::::::::::::::::::', counter2) // 0 부터 시작 +1
-
-    //? 1. 유저가 버튼을 클릭한다.
-    //? 2. counter+1 해서 1이 됨
-    //? 3. setState 함수 호출
-    //? 4. console.log실행됨
-    //? 5. 변수값은 1로 보이고, state 값은 아직 안변했기 때문에 그 전 값이 보인다.
-    //? 6. 함수 끝
-    //? 7. App 다시 리렌더링
-    //? 8. count = 0 을 거치며 새로 초기화되어 +1 부터 시작되어버린다.
-    //? 9. 업데이트 된 state값이 보인다.
+    // 결과
+    let resultString = judgement(choice[userChoice], pcChoice)
+    setResult(resultString)
   }
+
+  const randomChoice = () => {
+    let itemArray = Object.keys(choice);
+    let randomItem = Math.floor(Math.random()*itemArray.length); // Math.random() 은 0과 1 사이 0.44554545 .. 이런 값을 가져온다. 배열길이를 곱해주고, floor 해주면 0, 1, 2, 뽑을 수 있다.  
+    let final = itemArray[randomItem];
+    return choice[final];
+  }
+
+  const judgement = (user, pc) => {
+    if(user.name === pc.name) return 'tie';
+    else if(user.name==='Rock') return pc.name==='Scissors' ? 'win' : 'lose';
+    else if(user.name==='Scissors') return pc.name==='Paper' ? 'win' : 'lose';
+    else if(user.name==='Paper') return pc.name==='Rock' ? 'win' : 'lose';
+  }
+
+
 
   return (
     <div>
-      <div>{counter}</div>
-      <div>state: {counter2}</div>
-      <button onClick={increase}>click Here</button>
+      <div className='main'>
+        <Box title="you" item={userSelect} result={result} />
+        <Box title="computer" item={pcSelect} result={result} />
+      </div>
+      <div className='buttons'>
+        <button onClick={()=>play("scissors")}>가위</button>  {/* <button onClick={()=>play("")play("scissors")}>가위</button> 해버리면 함수를 바로 실행해버린다!  */ }
+        <button onClick={()=>play("rock")}>바위</button> {/* 함수를 꼭 콜백 함수의 형태로 넣어줘야 한다 */}
+        <button onClick={()=>play("paper")}>보</button>
+      </div>
+
     </div>
   );
 }
--- src/component/Box.js
+++ src/component/Box.js
@@ -1,10 +1,22 @@
 import React from 'react'
 
 const Box = (props) => {
+    let result;
+    if(
+        props.title === 'computer' &&
+        props.result !== 'tie' &&
+        props.result !== ''
+    ){
+        result = props.result === 'win' ? 'lose' : 'win';
+    }else{
+        result = props.result;
+    }
 
     return (
-        <div className='box'>
-            {props.num} - {props.name}
+        <div className={`box ${result}`}>
+            <h1>{props.title}</h1>
+            <img src={props.item && props.item.img} alt="" />  {/* 맨 처음 null 이라서 엣지케이스 가드를 넣어줘야 한다. */}
+            <h2>{result}</h2>
         </div>
     )
 }
